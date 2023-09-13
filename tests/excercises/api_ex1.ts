a43c40543075407435d0676f34fd0b78e078ea95
Eran ius <iusepolskyeran@gmail.com>
2023-09-13 12:50:21 +0300

    adding 2nd excercise + changes from first one

--- tests/excercises/api_ex1.ts
+++ tests/excercises/api_ex1.ts
@@ -21,16 +21,42 @@ const data1: myApi = {
     username: "user1",
     password:"password1"
 }
-  
+
+// user that exist but forbidden from logging in
+const data2: myApi = {
+    username: "user2",
+    password:"password2"
+}
+//STATUS CODES SUPPORT 
+export enum StatusCodes {
+    NOTFOUND = 404,
+    SUCCESS = 200,
+    ACCEPTED = 202,
+    FORBIDDEN = 403,
+    UNAUTHORIZED = 401
+  };
+
     //check input data
-    function checkUser(loginData:myApi) {
-        if(loginData.username !== "user1") {
+    function checkValidUser(loginData:myApi) {
+        if(loginData.username !== "user1"&&loginData.password !== "password1") {
+            return'{statusCodes.SUCCESS} status returned '
             console.log("wrong username")
         }else{
             if(loginData.password!="password1") 
-                console.log("wrong password")
+               return '${statusCodes.UNAUTHORIZED} status returned '
         }
         console.log("correct user")
     }
     
-    checkUser(data1)
+    function check403 (loginData:myApi) {
+        if(loginData.username == "user2"&&loginData.password=='password2') {
+            return`${StatusCodes.FORBIDDEN} status returned `
+          //  console.log("wrong username")
+           }
+        }
+        console.log ("Forbidden User test Finished ")
+
+
+    
+    checkValidUser(data1)
+    check403(data2)
